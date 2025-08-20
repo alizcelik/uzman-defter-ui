6d9ef94565e9c891415cb173a141931f1d56f97b
Ali Özçelik <143481544+alizcelik@users.noreply.github.com>
2025-08-20 16:57:39 +0300

    added: RouterLink

---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Single Project Page: {{ route.params.id }}</h1>
+    <RouterLink to="/projects">Back to Projects</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<style lang=""></style>
