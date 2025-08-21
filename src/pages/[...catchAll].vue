9132b2719155fb79fb6cde36a5caba48197194a2
Ali Özçelik <143481544+alizcelik@users.noreply.github.com>
2025-08-21 08:24:16 +0300

    auto router & supabes integration

---
+++ src/pages/[...catchAll].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+    <p>Path: {{ route.path }}</p>
+    <RouterLink to="/">Go to Home</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
---
+++ src/pages/projects/[id].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const params = route.params as { id: string | number }
+</script>
+
+<template>
+  <div>
+    <h1>Project {{ params.id }}</h1>
+  </div>
+</template>
