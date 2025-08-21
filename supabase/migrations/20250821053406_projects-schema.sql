bb4dd67611e5472f7d3729f94b066a24bc0d86b1
Ali Özçelik <143481544+alizcelik@users.noreply.github.com>
2025-08-21 21:16:21 +0300

    data seed with faker

---
+++ supabase/migrations/20250821053406_projects-schema.sql
@@ -0,0 +1,14 @@
+drop table if exists projects;
+
+drop type if exists status_type;
+create type status_type as enum ('in-progress', 'completed');
+
+create table
+  projects(
+    id bigint primary key generated always as identity not null,
+    created_at timestamptz default now() not null,
+    name text not null,
+    slug text unique not null,
+    status status_type default 'in-progress' not null,
+    collaborators text array default array[]::varchar[] not null
+  );
