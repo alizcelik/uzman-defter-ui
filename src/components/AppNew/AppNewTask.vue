ad5b07bd6f83e42b3fb5c7ca04dcb02631df5b59
Ali Özçelik <143481544+alizcelik@users.noreply.github.com>
2025-11-09 00:51:55 +0300

    feat: enhance task creation form with project and profile selection options

--- src/components/AppNew/AppNewTask.vue
+++ src/components/AppNew/AppNewTask.vue
@@ -1,6 +1,58 @@
 <script setup lang="ts">
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import { profilesQuery, projectsQuery } from '@/utils/supaQueries'
+import { ref } from 'vue'
 const sheetOpen = defineModel<boolean>()
+
+interface NewTaskForm {
+  taskTitle?: string
+  taskDescription?: string
+  dueDate?: string
+}
+
+type SelectOption = { label: string; value: number | string }
+
+const selectOptions = ref({
+  projects: [] as SelectOption[],
+  profiles: [] as SelectOption[],
+})
+
+const getProjectsOptions = async () => {
+  const { data: allProjects } = await projectsQuery
+
+  if (!allProjects) return
+
+  allProjects.forEach((project) => {
+    selectOptions.value.projects.push({
+      label: project.name,
+      value: project.id,
+    })
+  })
+}
+
+const getProfilesOptions = async () => {
+  const { data: allProfiles } = await profilesQuery
+
+  if (!allProfiles) return
+
+  allProfiles.forEach((profile) => {
+    selectOptions.value.profiles.push({
+      label: profile.full_name,
+      value: profile.id,
+    })
+  })
+}
+
+const createTask = (data: NewTaskForm) => {
+  console.log('Form submitted with data:', data)
+  // Here you can handle the form submission, e.g., send data to an API
+  sheetOpen.value = false // Close the sheet after submission
+}
+const getOptions = async () => {
+  await Promise.all([getProjectsOptions(), getProfilesOptions()])
+}
+
+getOptions()
 </script>
 
 <template>
@@ -10,7 +62,32 @@ const sheetOpen = defineModel<boolean>()
         <SheetTitle>Create new task</SheetTitle>
       </SheetHeader>
 
-      <FormKit type="text" id="new-task-form" class="space-y-4" />
+      <FormKit type="form" @submit="createTask" submit-label="Create Task">
+        <FormKit type="text" name="name" id="name" label="Name" placeholder="My new task" />
+        <FormKit
+          type="select"
+          name="for"
+          id="for"
+          label="For"
+          placeholder="Select a user"
+          :options="selectOptions.profiles"
+        />
+        <FormKit
+          type="select"
+          name="project"
+          id="project"
+          label="Porject"
+          placeholder="Select a project"
+          :options="selectOptions.projects"
+        />
+        <FormKit
+          type="textarea"
+          name="description"
+          id="description"
+          label="Description"
+          placeholder="Task description"
+        />
+      </FormKit>
     </SheetContent>
   </Sheet>
 </template>
--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -1,18 +1,17 @@
 import { supabase } from '@/lib/supabaseClient'
 import type { QueryData } from '@supabase/supabase-js'
 
-export const tasksWithProjectsQuery = supabase
-  .from('tasks')
-  .select(` * , projects (id, name, slug)`)
-
+export const tasksWithProjectsQuery = supabase.from('tasks').select(`
+    *,
+    projects (
+      id,
+      name,
+      slug
+    )
+  `)
 export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
-export const taskQuery = (id: number) =>
-  supabase.from('tasks').select(` * , projects (id, name, slug)`).eq('id', id).single()
-
-export type Task = QueryData<ReturnType<typeof taskQuery>>
-
-export const projectsQuery = supabase.from('projects').select('*')
+export const projectsQuery = supabase.from('projects').select()
 export type Projects = QueryData<typeof projectsQuery>
 
 export const projectQuery = (slug: string) =>
@@ -20,8 +19,13 @@ export const projectQuery = (slug: string) =>
     .from('projects')
     .select(
       `
-   * ,
-  tasks (id, name, status, due_date)
+   *,
+   tasks (
+    id,
+    name,
+    status,
+    due_date
+   )
   `,
     )
     .eq('slug', slug)
@@ -33,17 +37,34 @@ export const updateProjectQuery = (updatedProject = {}, id: number) => {
   return supabase.from('projects').update(updatedProject).eq('id', id)
 }
 
-export interface profilesQuery {
-  column: string
-  value: string | number
+export const taskQuery = (id: number) => {
+  return supabase
+    .from('tasks')
+    .select(
+      `
+      *,
+      projects (
+        id,
+        name,
+        slug
+      )
+    `,
+    )
+    .eq('id', id)
+    .single()
+}
+export type Task = QueryData<ReturnType<typeof taskQuery>>
+
+export const updateTaskQuery = (updatedTask = {}, id: number) => {
+  return supabase.from('tasks').update(updatedTask).eq('id', id)
 }
 
-export const profilesQuery = ({ column, value }: profilesQuery) => {
-  return supabase.from('profiles').select('*').eq(column, value).single()
+export const profileQuery = ({ column, value }: { column: string; value: string }) => {
+  return supabase.from('profiles').select().eq(column, value).single()
 }
 
-export const groupedProfilesQuery = (ids: string[]) => {
-  return supabase.from('profiles').select('username, avatar_url, id, full_name').in('id', ids)
-}
+export const profilesQuery = supabase.from('profiles').select(`id, full_name`)
 
+export const groupedProfilesQuery = (userIds: string[]) =>
+  supabase.from('profiles').select('username, avatar_url, id, full_name').in('id', userIds)
 export type Collabs = QueryData<ReturnType<typeof groupedProfilesQuery>>
