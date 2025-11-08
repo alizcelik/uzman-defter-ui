aabef330397a8abd56cfd3742ae9047c28aee0e5
Ali Özçelik <143481544+alizcelik@users.noreply.github.com>
2025-11-08 22:13:29 +0300

    feat: add new task sheet component and integrate with sidebar
    - Added `AppNewTask` component for creating new tasks.
    - Integrated task creation functionality into the `Sidebar` component with a dropdown menu.
    - Updated `AuthLayout` to manage the state of the task sheet.
    - Introduced `Sheet` components for the task creation UI, including header, content, footer, and overlay.
    - Updated dependencies in `package.json` for `@formkit/vue` and `reka-ui`.

---
+++ src/components/AppNew/AppNewTask.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+const sheetOpen = defineModel<boolean>()
+</script>
+
+<template>
+  <Sheet v-model:open="sheetOpen">
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Create new task</SheetTitle>
+      </SheetHeader>
+
+      <p>Content</p>
+    </SheetContent>
+  </Sheet>
+</template>
--- src/components/layout/main/AuthLayout.vue
+++ src/components/layout/main/AuthLayout.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
+import AppNewTask from '@/components/AppNew/AppNewTask.vue'
 import Sidebar from '@/components/layout/Sidebar.vue'
 import TopNavbar from '@/components/layout/TopNavbar.vue'
 import { usePageStore } from '@/stores/page'
 import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 
 const { pageData } = storeToRefs(usePageStore())
+const taskSheetOpen = ref(false)
 </script>
 
 <template>
-  <Sidebar />
+  <Sidebar @taskClicked="taskSheetOpen = true" />
   <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
     <TopNavbar />
+    <AppNewTask v-model="taskSheetOpen" />
+
     <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
       <div class="flex items-center">
         <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
